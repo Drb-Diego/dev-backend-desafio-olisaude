2e853d9f8de925a526ee64da1d41a0183d846ec5
Diego Ramos <diegorbrito9@gmail.com>
2022-12-19 15:13:41 -0300

    feat: add 'validationPipe' for automatic validation class-validator

--- dev/src/main.ts
+++ dev/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './entities/App/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const { PORT } = process.env;
 
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(PORT);
 }
 bootstrap();
